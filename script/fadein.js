241fe33091cd798830bef81d96a7345ff6347ac4
Justin Lung <34651215+JustinLung@users.noreply.github.com>
2019-06-25 09:17:28 +0200

    Delete fadein.js

--- script/fadein.js
+++
@@ -1,8 +0,0 @@
-const $el = document.querySelector('.content');
-const duration = 1;
-const from = {opacity: 0, ease: Linear.ease};
-const to = { opacity: 1};
-
-function fadeIn() {
-    TweenLite.fromT0($el, duration, from, to);
-}
